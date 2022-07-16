985bd79360473eb2e0de179de8ef2687b9cc91b4
RogelioClementeIMTC <ceinnova11442@innovaccion.mx>
2022-07-16 00:53:18 -0500

    First commit

---
+++ 1. Two Sum (Easy).cs
@@ -0,0 +1,25 @@
+﻿public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        int[] resultado = new int[2]; //Se instanciando la matriz del resultado.
+        
+        Dictionary<int, int> myDict = new Dictionary<int,int>();
+        
+        //** Ciclo for que evalua cada elemento de la matriz para encontrar el resultado **//
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (myDict.ContainsKey(target-nums[i])) 
+            {
+                 resultado[1]=i;
+                 resultado[0]=myDict[target-nums[i]];
+                 break;
+            }
+            
+            else if(!myDict.ContainsKey(nums[i])) myDict.Add(nums[i], i);
+        }
+        
+        //** Regresa el resultado **//
+        return resultado;
+    }
+}
---
+++ 977. Squares of a Sorted Array (Easy).cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        
+        int[] resultado = new int[nums.Length];
+        
+        //** Convertir a cuadrado **// 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = nums[i] * nums[i];
+        }
+        
+        //** Se organiza la matriz **//
+        Array.Sort(nums); 
+        resultado = nums;
+        return resultado;
+    }
+}
